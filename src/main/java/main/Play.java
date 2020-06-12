c75e21f30c3321f17e9d0a4673da2fd23b4f7260
Korbinian <kmr.ring@outlook.com>
2020-06-12 23:14:42 +0100

    improved efficiency

--- src/main/java/main/Play.java
+++ src/main/java/main/Play.java
@@ -8,36 +8,34 @@ public class Play {
 
     public Fields fields = PageFactory.initElements(Utils.driver, Fields.class);
     Actions actions = new Actions(Utils.driver);
+    public static  String[][] blackList = new String[20][20];
 
     public void play(){
-        fields.playField[2][2].click();
-        flagScanAll();
-        if (fields.playField[2][17]!=null && fields.playField[2][17].getAttribute("class").contains("hd_closed")){
-            fields.playField[2][17].click();
-            flagScanAll();
-        }
-        if (fields.playField[17][2]!=null && fields.playField[17][2].getAttribute("class").contains("hd_closed")){
-            fields.playField[17][2].click();
-            flagScanAll();
-        }
-        if (fields.playField[17][17]!=null && fields.playField[17][17].getAttribute("class").contains("hd_closed")){
-            fields.playField[17][17].click();
-            flagScanAll();
-        }
-        if (fields.playField[9][9]!=null && fields.playField[9][9].getAttribute("class").contains("hd_closed")){
-            fields.playField[9][9].click();
-            flagScanAll();
-        }
-        flagScanAll();
+        click(2,2);
+        click(2,17);
+        click(17,2);
+        click(17,17);
+        click(10,10);
         while(true){
             int x = (int) ((Math.random() * (17)) + 2);
             int y = (int) ((Math.random() * (17)) + 2);
-            if (fields.playField[x][y]!=null && fields.playField[x][y].getAttribute("class").contains("hd_closed")){
+            if (fields.playField[x][y]!=null && fields.playField[x][y].getAttribute("class").equals("cell size24 hd_closed")){
                 fields.playField[x][y].click();
-                flagScanAll();
+                flagScanA25(x,y);
             }
         }
 
+    }
+
+    public void click(int x,int y){
+        if (fields.playField[x][y]!=null && fields.playField[x][y].getAttribute("class").contains("hd_closed")){
+            fields.playField[x][y].click();
+            if (fields.playField[x][y].getAttribute("class").contains("0")){
+                flagScanAll();
+            } else {
+                flagScanA25(x, y);
+            }
+        }
     }
 
     public void flagCheck(int x, int y, int bombs){
@@ -45,7 +43,7 @@ public class Play {
         for (int tempx = x-1;tempx<x+2;tempx++){
             for (int tempy = y-1;tempy<y+2;tempy++){
                 if (fields.getPlayField()[tempx][tempy]==null ||
-                        fields.getPlayField()[tempx][tempy].getAttribute("class").equals("cell size24 hd_closed")){
+                        fields.getPlayField()[tempx][tempy].getAttribute("class").contains("closed")){
                     counter++;
                 }
             }
@@ -62,7 +60,8 @@ public class Play {
                         continue;
                     }
                     if (fields.getPlayField()[tempx][tempy].getAttribute("class").equals("cell size24 hd_closed")){
-                        fields.playField[tempx][tempy] = null;
+                        actions.moveToElement(fields.playField[tempx][tempy]).contextClick().build().perform();
+                        blackList[tempx][tempy] = "flag";
                         safeCheck(tempx,tempy);
                     }
                 }
@@ -73,34 +72,24 @@ public class Play {
     private void safeCheck(int x, int y) {
         for (int tempx = x-1;tempx<x+2;tempx++){
             for (int tempy = y-1;tempy<y+2;tempy++){
-                if (fields.getPlayField()[tempx][tempy]==null || fields.getPlayField()[tempx][tempy].getAttribute("class").equals("cell size24 hd_closed")){
+                if (fields.getPlayField()[tempx][tempy]==null || blackList[tempx][tempy]!=null || fields.getPlayField()[tempx][tempy].getAttribute("class").equals("cell size24 hd_closed")){
                     continue;
                 }
                 int counter = Integer.parseInt(String.valueOf(fields.getPlayField()[tempx][tempy].getAttribute("class").charAt(fields.getPlayField()[tempx][tempy].getAttribute("class").length()-1)));
                 for (int tempx2 = tempx-1;tempx2<tempx+2;tempx2++){
                     for (int tempy2 = tempy-1;tempy2<tempy+2;tempy2++){
-                        if (fields.getPlayField()[tempx2][tempy2]==null){
+                        if (fields.getPlayField()[tempx2][tempy2]==null ){
+                            continue;
+                        }
+                        if (fields.getPlayField()[tempx2][tempy2].getAttribute("class").contains("flag")){
                             counter--;
                         }
                     }
                 }
-                if ((tempx==17||tempx==2)&&(tempy==17||tempy==2)){
-                    counter+=5;
-                } else if (tempx==17||tempx==2||tempy==17||tempy==2){
-                    counter+=3;
-                }
                 if (counter==0){
-                    for (int tempx2 = tempx-1;tempx2<tempx+2;tempx2++){
-                        for (int tempy2 = tempy-1;tempy2<tempy+2;tempy2++){
-                            if (fields.getPlayField()[tempx2][tempy2]==null){
-                                continue;
-                            }
-                            if (fields.getPlayField()[tempx2][tempy2].getAttribute("class").equals("cell size24 hd_closed")){
-                                fields.playField[tempx2][tempy2].click();
-                                flagScanA25(tempx2,tempy2);
-                            }
-                        }
-                    }
+                    actions.moveToElement(fields.getPlayField()[tempx][tempy]).doubleClick().perform();
+                    blackList[tempx][tempy] = "safe";
+                    flagScanA25(tempx,tempy);
                 }
             }
         }
@@ -109,7 +98,7 @@ public class Play {
     public void flagScanA25(int x, int y) {
         for (int tempx = x-2;tempx<x+3;tempx++){
             for (int tempy = y-2;tempy<y+3;tempy++){
-                if (fields.getPlayField()[tempx][tempy]!=null && fields.getPlayField()[tempx][tempy].getAttribute("class").contains("opened") && !fields.getPlayField()[tempx][tempy].getAttribute("class").contains("0")) {
+                if (fields.getPlayField()[tempx][tempy]!=null && blackList[tempx][tempy]==null && fields.getPlayField()[tempx][tempy].getAttribute("class").contains("opened") && !fields.getPlayField()[tempx][tempy].getAttribute("class").contains("0")) {
                     int bombs = Integer.parseInt(String.valueOf(fields.getPlayField()[tempx][tempy].getAttribute("class").charAt(fields.getPlayField()[tempx][tempy].getAttribute("class").length() - 1)));
                     flagCheck( tempx,  tempy,  bombs);
                 }
@@ -121,7 +110,7 @@ public class Play {
     public void flagScanAll() {
         for (int y = 2; y < 17; y++) {
             for (int x = 2; x < 17; x++) {
-                if (fields.getPlayField()[x][y]!=null && fields.getPlayField()[x][y].getAttribute("class").contains("opened") && !fields.getPlayField()[x][y].getAttribute("class").contains("0")) {
+                if (fields.getPlayField()[x][y]!=null && blackList[x][y]==null && fields.getPlayField()[x][y].getAttribute("class").contains("opened") && !fields.getPlayField()[x][y].getAttribute("class").contains("0")) {
                     int bombs = Integer.parseInt(String.valueOf(fields.getPlayField()[x][y].getAttribute("class").charAt(fields.getPlayField()[x][y].getAttribute("class").length() - 1)));
                     flagCheck( x,  y,  bombs);
                 }
